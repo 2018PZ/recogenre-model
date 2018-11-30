e182d3d7277c7eb0dab139d9c0931f217d98536c
kirjava113 <annaluizalyjak@gmail.com>
2018-11-30 01:16:25 +0100

    creating train wav

--- src/main.py
+++ src/main.py
@@ -15,11 +15,13 @@ from tensorflow import keras
 import librosa
 
 from src.read_data import get_train_paths
+from src.read_data import get_wav_destinations
+from src.read_data import get_spec_destinations
 
 
 def convert_to_wav(input_path, output_path):
-    if os.path.isfile(output_file):
-        return 'File ' + output_file + ' already exists'
+    if os.path.isfile(output_path):
+        return 'File ' + output_path + ' already exists'
     ff = FFmpeg(inputs={input_path: None}, outputs={output_path: None})
     # ff.cmd
     ff.run()
@@ -55,32 +57,43 @@ def print_mel_spectrogram(output_file):
     return 'Painting plot...'
 
 
-def create_and_save_spectrogram(output_file):
-    spec = create_mel_spectrogram(output_file)
+def create_and_save_spectrogram(input_file, output_file):
+    spec = create_mel_spectrogram(input_file)
     plt.figure(figsize=(10, 4))
     librosa.display.specshow(librosa.power_to_db(spec, ref=np.max), fmax=8000)
 
-    name = output_file[:-4]
-    plt.savefig(name, bbox_inches='tight', pad_inches=-0.1, transparent=True, frameon=None, format=None)
-    return 'Save sectrogram to file: ' + name + '.png'
+    plt.savefig(output_file, bbox_inches='tight', pad_inches=-0.1, transparent=True, frameon=None, format=None)
+    return 'Save sectrogram to file: ' + output_file + '.png'
 
 
 # Preparing file
-path_to_file = pjoin('../data', 'die.au')
-# path_to_file = pjoin('../data', 'disco.00000.au')
-# path_to_file = pjoin('../data', 'metal.00000.au')
-output_file = path_to_file[:-2] + 'wav'
-result = convert_to_wav(path_to_file, output_file)
-print(result)
-
-# Painting spectrogram
-# print_MEL_spectrogram(output_file)
-
-create_and_save_spectrogram(output_file)
-
+# path_to_file = pjoin('../data', 'die.au')
+# output_file = path_to_file[:-2] + 'wav'
+# result = convert_to_wav(path_to_file, output_file)
+# print(result)
 #
-# train_au_path_list = get_train_paths()
-# # for file in path_list:
+# # Painting spectrogram
+# # print_MEL_spectrogram(output_file)
+#
+# create_and_save_spectrogram(output_file)
+
+
+train_au_path_list = get_train_paths()
+train_wav_path_list = get_wav_destinations()
+nuber_of_files = train_wav_path_list.__len__()
+
+for x in range(0, nuber_of_files):
+    convert_to_wav(train_au_path_list[x], train_wav_path_list[x])
+
+spectrograms = get_spec_destinations()
+
+print(spectrograms)
+
+for x in range(0, nuber_of_files):
+    create_and_save_spectrogram(train_wav_path_list[x], spectrograms[x])
+
+
+# for file in path_list:
 # file = train_au_path_list[1]
 # output = file[:-2] + 'wav'
 # print(output)
--- src/read_data.py
+++ src/read_data.py
@@ -15,6 +15,29 @@ def get_train_paths():
     return path_list
 
 
+def get_wav_destinations():
+    path = '../data/genres'
+    path_list = []
+    source = get_train_paths()
+
+    for s in source:
+        end = s.__len__()-2
+        path_list.append(path + s[15:end] + 'wav')
+
+    return path_list
+
+
+def get_spec_destinations():
+    path = '../data/spec'
+    path_list = []
+    source = get_train_paths()
+
+    for s in source:
+        end = s.__len__() - 2
+        path_list.append(path + s[15:end] + "png")
+
+    return path_list
+
 # Test
 # for p in get_train_paths():
 #     print(p)
